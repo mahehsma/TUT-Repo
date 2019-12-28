ceaaae510b36744e8e04456e07839940627d3c8b
mahehsma <56866320+mahehsma@users.noreply.github.com>
2019-12-28 20:50:14 +0100

    etst

--- PushPopStack/src/DArray.java
+++ PushPopStack/src/DArray.java
@@ -1,38 +1,38 @@
 
 public class DArray {
 	private char[] charArray;
-	private int size;
-	private int pointer=0;
-	boolean isEmpty=true;
+	private int sizeStack;
+	private int pointer = 0;
+	boolean isEmpty = true;
 
-	DArray(int s) { //constructor creates array (length 10)
-		this.size = s;
-		charArray = new char[size];
+	DArray(int s) { // constructor creates array (length 10)
+		this.sizeStack = s;
+		charArray = new char[sizeStack];
 	}
 
-	void set(char ch) {
-		if (pointer >= size) { //checks if the stack size is too small
-			char[] helpArray = new char[pointer + 10]; //increases stack size by 10
-			for (int i = 0; i < size; i++) {
+	void set(char bracket) {
+		if (pointer >= sizeStack) { // checks if the stack size is too small
+			char[] helpArray = new char[pointer + 10]; // increases stack size by 10
+			for (int i = 0; i < sizeStack; i++) {
 				helpArray[i] = charArray[i];
 			}
-			size = pointer + 10;
-			charArray = helpArray;
+			sizeStack = pointer + 10;
+			charArray = helpArray; // changes reference from helpArray to charArray
 		}
-		charArray[pointer] = ch; //pushes input (ch) at the position of pointer
+		charArray[pointer] = bracket; // pushes input (bracket) at the position of pointer
 		pointer++;
-		isEmpty=false;
+		isEmpty = false;
 	}
 
 	char get() {
 		pointer--;
-		if (pointer==0) {
-			isEmpty=true;
+		if (pointer == 0) {
+			isEmpty = true;
 		}
-		if (pointer < 0 ) {
+		if (pointer < 0) {
 			return '0';
 		} else {
-			return charArray[pointer];
+			return charArray[pointer]; //returns the bracket at position pointer
 		}
 
 	}
